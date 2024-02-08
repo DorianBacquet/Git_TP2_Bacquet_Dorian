6f582fa2d78fa5a766e23189b0a8fac71122a6ff
DorianBacquet <145097916+DorianBacquet@users.noreply.github.com>
2024-02-08 10:55:07 +0100

    Add files via upload

---
+++ tp2/Journée.java
@@ -0,0 +1,27 @@
+public class Journée {
+    private int epreuveMatin;
+    private int epreuveApresMidi;
+    private int epreuveSoiree;
+
+    public Journée(int epreuveMatin, int epreuveApresMidi, int epreuveSoiree, Auteur auteur){
+        this.epreuveMatin = epreuveMatin * auteur.getQualitéDrame();
+        this.epreuveApresMidi = epreuveApresMidi * auteur.getQualitéComédie();
+        this.epreuveSoiree = epreuveSoiree * auteur.getQualitéTragédie();
+    }
+
+    public int sommeScore(int epreuveMatin, int epreuveApresMidi, int epreuveSoiree) {
+        return epreuveMatin + epreuveApresMidi + epreuveSoiree;
+    }
+
+    public int getScoreMatin(){
+        return this.epreuveMatin;
+    }
+
+    public int getScoreApresMidi(){
+        return this.epreuveApresMidi;
+    }
+
+    public int getScoreSoiree(){
+        return this.epreuveSoiree;
+    }
+}
